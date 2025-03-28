a74ab4fbde67437af3f77a50a4207319aa1616d8
Emily Flion <emneo@kreog.com>
2025-03-28 19:18:21 +0100

    Remove flatpak exports from path
    Signed-off-by: Emily Flion <emneo@kreog.com>

--- .config/bash/path.sh
+++ .config/bash/path.sh
@@ -4,7 +4,4 @@ PATH=$HOME/.local/bin:$HOME/opt/bin:$PATH
 # Rust's way of doing things
 [ -f "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
 
-# Flatpaks
-PATH=/var/lib/flatpak/exports/bin/:$PATH
-
 export PATH
