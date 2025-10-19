a0768b2dffa18899515cffc4a18ae5e3a8a2db5e
Emily Flion <emneo@kreog.com>
2025-10-19 16:54:40 +0200

    Add npm use binaries to PATH
    Signed-off-by: Emily Flion <emneo@kreog.com>

--- .config/bash/path.sh
+++ .config/bash/path.sh
@@ -1,5 +1,5 @@
 # User specific environment
-PATH=$HOME/.local/bin:$HOME/opt/bin:$PATH
+PATH=$HOME/.local/bin:$HOME/.local/npm/bin:$HOME/opt/bin:$PATH
 
 # Rust's way of doing things
 [ -f "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
